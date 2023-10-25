d1a494192318e42535f2400821d631e3caf289e0
Tim Pei <timpei123@gmail.com>
2023-10-25 15:39:10 +0800

    paved way for create user unit test

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 
 require (
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.17 // indirect
--- storage/databaseStorageImpl_test.go
+++ storage/databaseStorageImpl_test.go
@@ -1,32 +1,62 @@
 package storage
 
 import (
+	"reflect"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/timdin/vfs/model"
 )
 
 func TestDBImpl_Register(t *testing.T) {
+	defer TeardownTestDB()
+	db := InitTestDB()
 
-	type args struct {
-		name string
+	testUserName := "user"
+	expectedUser := &model.User{
+		Name: testUserName,
 	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{
-			name:    "test",
-			args:    args{name: "test"},
-			wantErr: false,
-		},
+	if err := db.Register(testUserName); err != nil {
+		t.Error(err)
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			db := InitTestDB()
-			if err := db.Register(tt.args.name); (err != nil) != tt.wantErr {
-				t.Errorf("DBImpl.Register() error = %v, wantErr %v", err, tt.wantErr)
+	actualUser := &model.User{}
+	if err := db.lookUpUser(testUserName, actualUser); err != nil {
+		t.Error(err)
+	}
+
+	if compareStructIgnoreEmptyValues(*expectedUser, *actualUser) != true {
+		t.Error(cmp.Diff(expectedUser, actualUser))
+	}
+
+	if err := db.lookUpUser("not-a-name", actualUser); err == nil {
+		t.Error("should return error when looking up non-existent user")
+	}
+}
+
+// since we could not predict the value of automatically generated fields, i.e. ID
+// provide a helper function to compare two structs ignoring the empty fields in the expected param
+func compareStructIgnoreEmptyValues(expected, actual interface{}) bool {
+	// Use reflection to get the type of the model
+	modelType := reflect.TypeOf(expected)
+
+	for i := 0; i < modelType.NumField(); i++ {
+		field := modelType.Field(i)
+
+		// Check if the field is unexported (lowercase)
+		if field.PkgPath == "" {
+			fieldName := field.Name
+			fieldValue1 := reflect.ValueOf(expected).FieldByName(fieldName)
+			fieldValue2 := reflect.ValueOf(actual).FieldByName(fieldName)
+
+			// Check if the field value is not the zero value (default value)
+			if !reflect.DeepEqual(fieldValue1.Interface(), reflect.Zero(fieldValue1.Type()).Interface()) {
+				// Compare the field values
+				if !reflect.DeepEqual(fieldValue1.Interface(), fieldValue2.Interface()) {
+					return false
+				}
 			}
-			TeardownTestDB()
-		})
+		}
 	}
+
+	return true
 }
